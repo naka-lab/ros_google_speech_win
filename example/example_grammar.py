88afad9b13eb6a3193d5ec85c16d151a2260133a
なかとも <20672947+naka-tomo@users.noreply.github.com>
2023-03-05 15:24:53 +0900

    fix example

--- example/example_grammar.py
+++ example/example_grammar.py
@@ -7,7 +7,7 @@ os.environ["ROS_MASTER_URI"] = "http://192.168.0.157:11311"
 os.environ["ROS_IP"] = "192.168.0.150"
 
 # パスを通す
-sys.path.append( "../roslib" )
+sys.path.append("../ros_win/python_package")
 
 import rospy
 from std_msgs.msg import String
@@ -41,33 +41,35 @@ person3 : 太郎
 
 """
 
+
 def main():
-    rospy.init_node('gspeech_example2')
-    pub_grammar = rospy.Publisher( "grammar_lu/grammar", String, queue_size=10, latch=True )
-    pub_synthesis = rospy.Publisher( "google_speech/utterance", String, queue_size=10 )	
+    rospy.init_node("gspeech_example2")
+    pub_grammar = rospy.Publisher(
+        "grammar_lu/grammar", String, queue_size=10, latch=True
+    )
+    pub_synthesis = rospy.Publisher("google_speech/utterance", String, queue_size=10)
 
     time.sleep(2)
-    pub_grammar.publish( grammar )
+    pub_grammar.publish(grammar)
 
     while not rospy.is_shutdown():
-        msg = rospy.wait_for_message( 'grammar_lu/results',  String )
+        msg = rospy.wait_for_message("grammar_lu/results", String)
         results = yaml.safe_load(msg.data)
 
-        print( results["text"] )
-        print( results["gram_id"] )
-        print( results["slot_id"] )
-        print( results["slot_str"] )
+        print(results["text"])
+        print(results["gram_id"])
+        print(results["slot_id"])
+        print(results["slot_str"])
         print("--------")
 
-        if results["gram_id"]=="greeding":
-            if results["slot_id"][0]=="hello":
+        if results["gram_id"] == "greeding":
+            if results["slot_id"][0] == "hello":
                 pub_synthesis.publish("ごきげんよう")
-            elif results["slot_id"][0]=="bye":
+            elif results["slot_id"][0] == "bye":
                 pub_synthesis.publish("さようなら")
-            elif results["slot_id"][0]=="morning":
+            elif results["slot_id"][0] == "morning":
                 pub_synthesis.publish("早起きは苦手です")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
